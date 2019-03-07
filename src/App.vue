3e4856e40ef0bd7b2b3e11734b5258057e33035f
caoqingwei <caoqingwei@alltuu.com>
2019-03-07 15:27:07 +0800

    3月7日test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/v"></router-link>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,65 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="files">点击上传</div>
+  </div>  
 </template>
 
 <script>
+import plupload from "plupload"
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      uploader:{},
+    }
+  },
+  mounted(){
+    this.uploadFiles()
+  },
+  methods:{
+    uploadFiles(){
+      this.uploader = new plupload.Uploader({
+        browse_button:"files",
+        filters:{
+          mime_types:[
+            {
+              title:"Image files",
+              extensions:"jpg,gif,png"
+            }
+          ],
+          max_file_size:"400kb", //最大只能上传400kb的文件
+          prevent_duplicates:true //不允许选取重复文件
+        }
+      });
+      this.uploader.init();
+      this.uploader.bind("FilesAdded",(up,files) => {
+        console.log("文件添加到上传队列")
+      }),
+      this.uploader.bind("BeforeUpload",(up, file) => {
+        console.log("文件准备上传")
+      }),
+      this.uploader.bind("UploadProgress",(up,file) => {
+        console.log("文件正在上传中")
+      }),
+      this.uploader.bind("FileUploaded",(up,file,res) => {
+        console.log("文件上传完成")
+      })
+      this.uploader.bind("Error",(code,file,message) => {
+        console.log("文件上传错误")
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="less" scoped>
+.hello{
+  #files{
+    cursor: pointer;
+  }
 }
 </style>
