3a175b51a79b837697922723cf08d02f0ba86496
caoqingwei <caoqingwei@alltuu.com>
2019-03-08 18:34:25 +0800

    只差最后一步

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <button @click="movediv">{{move.value}}</button>
+    <div class="buttons">
+      <button @click="movediv" v-show="!ismove">移动</button>
+      <button @click="sureMove" v-show="ismove">确认</button>
+      <button @click="notMove" v-show="ismove">取消</button>
+    </div>
     <div v-for="(item,index) in divList" :key="index" :class="[{'active':item.active},{'target':index == targetIndex}]" @click="isactive(index)">{{item.value}}</div>
   </div>
 </template>
@@ -22,43 +26,87 @@ export default {
         {value:5,active:false},
       ],
       targetIndex:-1,
-      move:{
-        ismove:false,
-        value:"移动"
-      },
+      ismove:false,
       moveList:[]
     }
   },
   methods:{
     isactive(index){
-      if(this.move.ismove){
-        this.targetIndex = index;
-      } else{
-        var a = this.divList[index]
+      const a = this.divList[index]
+      if(this.ismove){
+        if(!a.active){
+            if(this.targetIndex == index){
+            this.targetIndex = -1;
+          } else{
+            this.targetIndex = index;
+          } 
+        } else {
+          alert("这张是要移动的")
+        }           
+      } else{  
         this.divList[index].active = !a.active
+        let haveIt = this.hasItem(index)     
         if(a.active){
-          console.log(this.hasItem(index))
-          if(this.hasItem(index)){
-            console.log("移动队列中的"+item.value)
-            return;
-          } else {
-            this.moveList.push(this.divList[index])
+          if(!haveIt){
+            this.moveList.push(a)
+            console.log("数组中没有" + this.moveList)
+          } 
+        }else{
+          if(haveIt){
+            this.moveList.splice(this.moveList.indexOf(a),1)
+            console.log("数组中有了" + this.moveList)
           }
         }
       }
-      
+      console.log("最终的逻辑实现"+ this.moveList)
     },
     movediv(){
-      this.move.ismove = !this.move.ismove
-      this.move.value = this.move.ismove?"取消":"移动";
-      if(!this.move.ismove) {
+      const willMove = this.divList.some((item) => {
+        return item.active
+      })
+      if(!willMove){
+        alert("请选择要移动的图片")
+        return
+      }
+      this.ismove = !this.ismove
+      if(!this.ismove) {
         this.targetIndex = -1;
       }
     },
     hasItem(index){
-      this.moveList.some((item) => {
-            return item.value == this.divList[index].value
-          })
+      let count = this.divList[index].value
+      return this.moveList.some(function(item){
+        console.log(item.value)
+        console.log(count)
+        return item.value == count
+      })
+    },
+    sureMove(){
+      const target = this.divList
+      if(this.targetIndex == -1){
+        alert("请选择要移动的位置")
+        return
+      } else {
+        this.ismove = !this.ismove
+        for(var i = 0; i < this.moveList.length;i++){
+          const item = this.moveList[i]
+          this.divList.splice(this.divList.indexOf(item),1)
+        }
+        if(this.divList.indexOf(target[this.target]) == 0){
+          this.divList.unshift(...this.moveList)
+        }else{
+          this.divList.splice(this.divList.indexOf(target[this.target]),0,...this.moveList)
+        }
+        console.log(this.divList)
+        for(var i = 0; i < this.divList.length; i++){
+          this.divList[i].active = false;
+          this.targetIndex = -1
+        }
+      }
+    },
+    notMove(){
+      this.ismove = !this.ismove
+      this.targetIndex = -1
     }
   }
 }
@@ -83,6 +131,14 @@ div{
   color:#fff;
   margin-top:20px;
   cursor: default;
+}
+.buttons{
+  width:100px;
+  background-color: #fff;
+  overflow: hidden;
+}
+button{
+  float:left
 }
 .active{
   border: 5px solid skyblue;
